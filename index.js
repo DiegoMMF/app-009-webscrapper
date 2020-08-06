be31e02873d3fce2093e71b427a0e734292abb39
DiegoMMF <diegomaldinifreyre@gmail.com>
2020-08-06 19:22:54 -0300

    renombramos app x index y llevamos a root

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Koa = require('koa');
 const KoaRouter = require("koa-router");
 const cors = require("@koa/cors");
 const bodyParser = require('koa-bodyparser');
-const scrapeMeThis = require('./scrapingThemistoWay/scrapeMeThis');
+const scrapeMeThis = require('./src/scrapingThemistoWay/scrapeMeThis');
 
 const app = new Koa();
 const router = new KoaRouter();
@@ -25,4 +25,4 @@ router.get("/", async (ctx, next) => {
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(3000, () => console.log("Themisto inicializado."));
+app.listen(process.env.PORT, () => console.log("Themisto inicializado."));
